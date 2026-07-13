7ad521082628c09e61626bf1f70ff1ab1cc87d3a
Sihle Sawula <150780503+sihlesawula@users.noreply.github.com>
2026-07-13 21:04:27 +0200

    re edit

--- js/script.js
+++ js/script.js
@@ -2,29 +2,31 @@ document.getElementById("hamburger-icon").addEventListener("click", function() {
     document.getElementById("nav-links").classList.toggle("active");
 });
 
-const dayIndex = getDayOfYear() % verses.length;
-const verseText = verses[dayIndex];
-
 document.addEventListener("DOMContentLoaded", () => {
-
-    document.getElementById("dailyVerse").textContent = verseText;
 
     const trigger = document.getElementById("verseTrigger");
     const modal = document.getElementById("verseModal");
-    const closeBtn = document.querySelector(".close-modal");
+    const closeBtn = document.getElementById("closeModal");
 
     trigger.addEventListener("click", () => {
         modal.classList.add("active");
+        document.body.style.overflow = "hidden";
     });
 
     closeBtn.addEventListener("click", () => {
         modal.classList.remove("active");
+        document.body.style.overflow = "auto";
     });
 
     modal.addEventListener("click", (e) => {
-        if (e.target === modal) {
+
+        if(e.target === modal){
+
             modal.classList.remove("active");
+            document.body.style.overflow = "auto";
+
         }
+
     });
 
 });
