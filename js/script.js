9a7bac9fe2f1c35c644296a033f130e14a2ccd68
Sihle Sawula <150780503+sihlesawula@users.noreply.github.com>
2026-07-13 21:38:23 +0200

    push new code

--- js/script.js
+++ js/script.js
@@ -2,36 +2,20 @@ document.getElementById("hamburger-icon").addEventListener("click", function() {
     document.getElementById("nav-links").classList.toggle("active");
 });
 
+const floatingImage = document.getElementById("floatingImage");
+const imageModal = document.getElementById("imageModal");
+const closeModal = document.querySelector(".close-modal");
 
-document.addEventListener("DOMContentLoaded", () => {
-
-    const trigger = document.getElementById("verseTrigger");
-    const modal = document.getElementById("verseModal");
-    const closeBtn = document.getElementById("closeModal");
-
-    trigger.addEventListener("click", () => {
-        modal.classList.add("active");
-        document.body.style.overflow = "hidden";
-    });
-
-    closeBtn.addEventListener("click", () => {
-        modal.classList.remove("active");
-        document.body.style.overflow = "auto";
-    });
-
-    modal.addEventListener("click", (e) => {
-
-        if(e.target === modal){
-
-            modal.classList.remove("active");
-            document.body.style.overflow = "auto";
-
-        }
-
-    });
-
+floatingImage.addEventListener("click", () => {
+    imageModal.classList.add("show");
 });
 
+closeModal.addEventListener("click", () => {
+    imageModal.classList.remove("show");
+});
 
-
-
+imageModal.addEventListener("click", (e) => {
+    if(e.target === imageModal){
+        imageModal.classList.remove("show");
+    }
+});
